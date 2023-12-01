9d437e59b5afed026f6290791eed4a0f05198980
Stephan D Ramalho <sramalho@fordham.edu>
2023-12-01 15:15:21 -0500

    Feature: Implement new custom colors
    Create main-yellow and main-gray colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,21 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        'main-yellow': '#FFC700',
+        'main-gray': '#F7F7F7'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
