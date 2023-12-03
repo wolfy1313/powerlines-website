4f82fb8b2aa8026f3335eafbe7f71e37dd540a01
Stephan D Ramalho <sramalho@fordham.edu>
2023-12-03 16:16:01 -0500

    Add HomeHeader to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import HomeHeader from '@/components/HomeHeader'
 import React from 'react'
 
 function HomePage() {
-  return <div>Home</div>
+  return (
+    <section className="flex flex-col my-0 min-w-screen max-w-screen">
+      <HomeHeader />
+    </section>
+  )
 }
 
 export default HomePage
