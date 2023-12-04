f5cfb410ddb9d3b54a3fd4e568093dfd658ef024
jeremyHarrell <hillvulf@gmail.com>
2023-12-04 16:45:06 -0600

    commit to checkout without merge conflict.

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+import { Josefin_Sans, Playfair_Display } from 'next/font/google'
+
+const josefinSans = Josefin_Sans({ subsets: ['latin'] })
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 function WhoWeAre() {
   return (
-    <div>WhoWeAre</div>
+    <section></section>
   )
 }
 
