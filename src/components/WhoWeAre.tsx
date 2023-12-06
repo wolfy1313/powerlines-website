660f586c5143d8cb604367175a6214094ca659bc
jeremyHarrell <hillvulf@gmail.com>
2023-12-06 13:41:08 -0600

    Complete Headline Arrangement. Add p tag and text

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -9,16 +9,20 @@ function WhoWeAre() {
   return (
     <section className="min-w-screen max-w-screen">
       <section className="flex flex-row justify-center">
-        <section className='flex flex-row font-bold text-lg md:text-5xl mt-8 md:mt-16'>
+        <section className='flex flex-row font-bold text-2xl md:text-3xl mt-8 md:mt-16'>
           <h2 className={`flex flex-row ${josefinSans.className}`}>WHO WE ARE</h2>
         </section>
       </section>
 
-      <section className='flex flex-row justify-center font-bold text-2xl md:text-5xl mt-8 md:mt-16'>
-        <h1 className={`flex flex-col whitespace-pre-line ${josefinSans.className}`}>
-          OUR FOCUS IS BRINGING
+      <section className='flex flex-row justify-center'>
+        <h1 className={`font-bold text-5xl ml-auto mr-auto md:text-6xl mt-8 md:mt-16  ${josefinSans.className}`}>
+          OUR FOCUS IS BRINGING <br />
+          <span className='text-main-yellow'>POWER </span>TO THE PUBLIC
+        </h1>
+      </section>
 
-          <span className='text-main-yellow px-3'>POWER</span>TO THE PUBLIC</h1>
+      <section className='flex flex-row justify-center'>
+        <p className={`text-black-400`}>PowerLines is a non-governmental organization that is passionate about restructuring the energy system. We seek to create a world in which clean, affordable, and reliable energy systems contribute to safe, healthy, and vibrant communities.</p>
       </section>
 
 
