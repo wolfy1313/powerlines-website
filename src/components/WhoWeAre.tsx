1f30167c0ae542890f2144cdbf91afa46ebaca1d
jeremyHarrell <hillvulf@gmail.com>
2023-12-06 09:44:02 -0600

    Arrange Headlines on screen

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -7,7 +7,26 @@ const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 function WhoWeAre() {
   return (
-    <section></section>
+    <section className="min-w-screen max-w-screen">
+      <section className="flex flex-row justify-center">
+        <section className='flex flex-row font-bold text-lg md:text-5xl mt-8 md:mt-16'>
+          <h2 className={`flex flex-row ${josefinSans.className}`}>WHO WE ARE</h2>
+        </section>
+      </section>
+
+      <section className='flex flex-row justify-center font-bold text-2xl md:text-5xl mt-8 md:mt-16'>
+        <h1 className={`flex flex-col whitespace-pre-line ${josefinSans.className}`}>
+          OUR FOCUS IS BRINGING
+
+          <span className='text-main-yellow px-3'>POWER</span>TO THE PUBLIC</h1>
+      </section>
+
+
+
+
+
+
+    </section>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import HomeHeader from '@/components/HomeHeader'
+import WhoWeAre from '@/components/WhoWeAre'
 import React from 'react'
 
 function HomePage() {
   return (
     <section className="flex flex-col my-0 min-w-screen max-w-screen">
       <HomeHeader />
+      <WhoWeAre />
     </section>
   )
 }
