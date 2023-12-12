3c5e51a6a2441a8da780b5d46ff407d3f3b72a7e
jeremyHarrell <hillvulf@gmail.com>
2023-12-12 10:40:44 -0600

    add PageHeroBG.svg to background of Our Work section

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -8,7 +8,7 @@ const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 function OurWork() {
   return (
     <section className=''>
-      <section className=''>
+      <section className="bg-[url('/PageHeroBG.svg')] h-319">
         <h1 className={`font-bold text-2xl md:text-3xl mt-8 md:mt-16 justify-center items-center text-center ${josefinSans.className}`}>OUR WORK</h1>
       </section>
       <section className='flex'>
