4660b956e886d9a82b9e08e760678c896a37ce8b
jeremyHarrell <hillvulf@gmail.com>
2023-12-12 10:04:56 -0600

    Add text to OurWork.tsx

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -6,12 +6,20 @@ const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 function OurWork() {
   return (
-    <section className='min-w-screen max-w-screen'>
-      <section className='flex flex-row justify-center'>
-        <h1 className={`justify-center text-center ${josefinSans.className}`}>OUR WORK</h1>
-
+    <section className=''>
+      <section className=''>
+        <h1 className={`font-bold text-2xl md:text-3xl mt-8 md:mt-16 justify-center items-center text-center ${josefinSans.className}`}>OUR WORK</h1>
       </section>
+      <section>
+        <section>
+          <h2>THE PROBLEM</h2>
+          <h2>Public utilities commissions have traditionally slowed the clean energy transition.</h2>
+          <h3>PUC’s approving utilities’ fossil fuel investments has become an economic, environmental, and health imperative. Utilities have operated with no competition and weak regulation pushing for affordable energy bills, sustainable generation, and reliable service. </h3>
+        </section>
+        <section>
 
+        </section>
+      </section>
     </section>
   )
 }
