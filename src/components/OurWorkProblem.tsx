217b09f94e38832764de008ec1d0716b7419d04a
jeremyHarrell <hillvulf@gmail.com>
2023-12-13 09:35:20 -0600

    update styling on OurWorkProblem.tsx

--- src/components/OurWorkProblem.tsx
+++ src/components/OurWorkProblem.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Josefin_Sans, Playfair_Display } from 'next/font/google'
+
+const josefinSans = Josefin_Sans({ subsets: ['latin'] })
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 function OurWorkProblem() {
   return (
     <div>
-      <section className='flex flex-row'>
+      <section className='flex flex-row mx-10'>
         <section>
-          <h2 className='text-darker-yellow pb-2 pt-2'>THE PROBLEM</h2>
-          <h2>Public utilities commissions have traditionally slowed the clean energy transition.</h2>
-          <h3>PUC’s approving utilities’ fossil fuel investments has become an economic, environmental, and health imperative. Utilities have operated with no competition and weak regulation pushing for affordable energy bills, sustainable generation, and reliable service. </h3>
+          <h2 className='text-darker-yellow font-bold text-2xl md:text-3xl pb-6 pt-8'>THE PROBLEM</h2>
+          <h2 className={`font-bold text-xl md:text-2xl pb-4 tracking-wider ${josefinSans.className}`}>Public utilities commissions have traditionally slowed the clean energy transition.</h2>
+          <h3 className={`texl-lg md text:xl tracking-wider ${playfairDisplay.className}`}>PUC’s approving utilities’ fossil fuel investments has become an economic, environmental, and health imperative. Utilities have operated with no competition and weak regulation pushing for affordable energy bills, sustainable generation, and reliable service. </h3>
         </section>
       </section>
     </div>
