1f21250fe10e4c093d9dd632927f8da677d1fb62
jeremyHarrell <hillvulf@gmail.com>
2023-12-13 09:37:01 -0600

    update margin-x  on OurWorkImage.tsx

--- src/components/OurWorkImage.tsx
+++ src/components/OurWorkImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function OurWorkImage() {
   return (
-    <section className=''>
+    <section className='mx-10'>
       <Image src="./ProblemImage.svg" width={2100} height={472} alt="image of wind turbines and solar panels" />
     </section>
   )
