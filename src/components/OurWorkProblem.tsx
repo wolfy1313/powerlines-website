4df2d219edb44f2ef597a1ee69c3e36b88e3890b
jeremyHarrell <hillvulf@gmail.com>
2023-12-14 14:21:03 -0600

    style our problem and our solution sections. download arrow image for dropdown

--- src/components/OurWorkProblem.tsx
+++ src/components/OurWorkProblem.tsx
@@ -7,8 +7,8 @@ const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 function OurWorkProblem() {
   return (
     <div>
-      <section className='flex flex-row mb-10'>
-        <section>
+      <section className='flex flex-row mb-10 '>
+        <section className=''>
           <h2 className='text-darker-yellow font-bold text-xl md:text-2xl pb-6 pt-8'>THE PROBLEM</h2>
           <h2 className={`f font-semibold text-xl md:text-2xl pb-4 tracking-normal ${josefinSans.className}`}>Public utilities commissions have traditionally slowed the clean energy transition.</h2>
           <h3 className={`texl-lg md text:xl tracking-normal ${playfairDisplay.className}`}>PUC’s approving utilities’ fossil fuel investments has become an economic, environmental, and health imperative. Utilities have operated with no competition and weak regulation pushing for affordable energy bills, sustainable generation, and reliable service. </h3>
--- src/pages/OurWorkPage.tsx
+++ src/pages/OurWorkPage.tsx
@@ -11,25 +11,26 @@ function OurWorkPage() {
   return (
     <section className='flex flex-col '>
 
-      {/* {Our Problem Section} */}
       <section className='m-0'>
         <OurWorkHeader />
       </section>
-      <section className='flex max-w-screen pt-7 mx-10'>
-        <section className='pr-4'>
+
+      {/* {Our Problem Section} */}
+      <section className='flex max-w-screen px-10 py-10'>
+        <section className='px-7 flex items-center' >
           <OurWorkProblem />
         </section>
-        <section className='pl-4 mx-10'>
+        <section className='px-8 mx-10'>
           <OurWorkImage />
         </section>
       </section>
 
       {/* {Our Solution Section} */}
-      <section className='flex max-w-screen pt-7 mx-10 py-6 bg-light-yellow'>
-        <section className='pr-4'>
+      <section className='flex max-w-screen py-10 px-10 bg-light-yellow'>
+        <section className='px-8 mx-10 '>
           <OurWorkSolutionImage />
         </section>
-        <section className='pl-4 mx-10'>
+        <section className='px-7 flex items-center' >
           <OurWorkSolution />
         </section>
       </section>
