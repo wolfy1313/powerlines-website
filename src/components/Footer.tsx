56162c32ce990f5f31d025625c206e8223a18471
Stephan D Ramalho <sramalho@fordham.edu>
2023-12-14 15:21:58 -0500

    Apply styling for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,12 +8,12 @@ const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 function Footer() {
   return (
-    <section className="flex flex-col justify-center h-auto min-w-screen max-w-screen bg-main-gray py-10 lg:h-72 lg:flex-row">
+    <section className="flex flex-col justify-center h-auto min-w-screen max-w-screen items-center bg-main-gray py-10 lg:h-72 lg:flex-row">
       {/* Left hand side of Footer */}
       <section
-        className={`flex flex-row flex-1 w-full justify-center ${josefinSans.className}`}
+        className={`flex flex-col flex-1 items-center min-w-screen justify-center pb-3 align-middle md:flex-row md:pb-0 ${josefinSans.className}`}
       >
-        <section className="relative flex flex-col justify-center h-[200px] w-[200px] mr-6 md:h-[230px] md:w-[240px]">
+        <section className="relative flex flex-col justify-center h-[100px] w-[100px] mr-6 md:h-[230px] md:w-[240px]">
           <Image src="/logo.svg" fill alt="logo" className="md: mt-3" />
         </section>
         {/* Text Container */}
@@ -35,7 +35,7 @@ function Footer() {
 
       {/* Right hand/Bottom side of Footer */}
       <section
-        className={`flex flex-1 flex-row  justify-center  my-auto text-2xl ${josefinSans.className} border-t pt-4 border-t-black lg:border-t-0 lg:border-l lg:border-l-black lg:pt-0`}
+        className={`flex flex-1 flex-col items-center  justify-center  my-auto text-2xl ${josefinSans.className} border-t pt-4 border-t-black md:flex-row lg:border-t-0 lg:border-l lg:border-l-black lg:pt-0`}
       >
         {/* Nav List */}
         <ul className="flex flex-col space-y-3 pt-2 pl-2 md:pr-16 lg:pr-0 lg:pt-0 lg:pl-32">
@@ -62,8 +62,8 @@ function Footer() {
         </ul>
 
         {/* Social Media */}
-        <section className="flex flex-col justify-center md:pl-32 lg:pl-32">
-          <h3>Stay Connected</h3>
+        <section className="flex flex-col justify-center pt-2 md:pt-0 md:pl-32 lg:pl-32">
+          <h3 className="hidden md:block">Stay Connected</h3>
 
           {/* Link Container */}
           <section className="flex flex-row space-x-10 pt-3">
