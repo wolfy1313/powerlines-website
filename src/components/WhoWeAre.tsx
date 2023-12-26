86de1ae8aeb99063e5e84a9b5dc16687fb5bc024
Stephan D Ramalho <sramalho@fordham.edu>
2023-12-26 13:29:46 -0500

    Refactor component to be forward Ref

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 import { Josefin_Sans, Playfair_Display } from 'next/font/google'
 
 const josefinSans = Josefin_Sans({ subsets: ['latin'] })
 const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
-function WhoWeAre() {
+const WhoWeAre = React.forwardRef<HTMLDivElement>((_props, ref) => {
   return (
-    <section className="min-w-screen max-w-screen bg-no-repeat bg-[url('/WhoWeAreBG.svg')] bg-cover">
+    <section
+      ref={ref}
+      className="min-w-screen max-w-screen bg-no-repeat bg-[url('/WhoWeAreBG.svg')] bg-cover text-center"
+    >
       <section className="flex flex-row justify-center">
         <section className="font-bold text-2xl md:text-3xl mt-8 md:mt-16">
           <h2 className={`${josefinSans.className}`}>WHO WE ARE</h2>
@@ -17,14 +18,14 @@ function WhoWeAre() {
 
       <section className="flex flex-row justify-center">
         <h1
-          className={`text-center font-bold text-5xl ml-auto mr-auto md:text-6xl mt-8 md:mt-16  ${josefinSans.className}`}
+          className={`text-center font-bold text-5xl ml-auto mr-auto md:text-6xl mt-8 md:mt-16 ${josefinSans.className}`}
         >
           OUR FOCUS IS BRINGING <br />
-          <span className="text-main-yellow">POWER </span>TO THE PUBLIC
+          <span className="text-main-yellow">POWER</span> TO THE PUBLIC
         </h1>
       </section>
 
-      <section className="flex flex-row flex-1  my-auto justify-center">
+      <section className="flex flex-row flex-1 my-auto justify-center">
         <section
           className={`${playfairDisplay.className} text-sm my-auto ml-5 mt-10 md:text-xl md:tracking-wider md:min-w-fit md:ml-0 min-w-screen`}
         >
@@ -37,13 +38,16 @@ function WhoWeAre() {
           </h3>
         </section>
       </section>
+
       <section className={`text-center ${josefinSans.className} mt-10 mb-20 `}>
-        <button className="bg-main-yellow rounded-full my-auto mx-auto px-14 py-2">
-          <Link href="/about-us">ABOUT US</Link>
+        <button className="bg-main-yellow rounded-full my-auto mx-auto px-14 py-2 font-bold">
+          ABOUT US
         </button>
       </section>
     </section>
   )
-}
+})
+
+WhoWeAre.displayName = 'WhoWeAre'
 
 export default WhoWeAre
