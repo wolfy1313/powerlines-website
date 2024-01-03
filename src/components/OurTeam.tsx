c304b274d9ba5bdea815586b77ea390e3bb57f11
Shondriane <shondriane.wise@yahoo.com>
2024-01-03 13:33:37 -0800

    updated ourTeam to include the background color and made the picture transparent

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -17,112 +17,95 @@ function OurTeam() {
         <p className={`text-[20px] text-center font-normal leading-9 mt-14 mb-24 mr-16 ml-16 ${playfairDisplay.className}`}>Our passionate and enthusiastic team ready to change the energy system.</p>
       </section>
     </section>
-    <section className="grid grid-cols-2 md:grid-cols-3 gap-2 justify-items-center mx-auto pt-2 md:pt-3 mb-10 max-w-6xl">
+   
 
     
-    <section className="mb-4 flex-1 ml-2 mr-2
-    h-[197px] md:h-[326px]">
-    <section className="relative 
-      overflow-hidden max-h-full">
-      <Image src="/femaleOutline2.png" width="295" height="326" alt="other image" />
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2 ">
-        <p className="text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small text-white mx-2">Role</p>
-      </section>
+    <section className="grid grid-cols-2  md:grid-cols-3 gap-2 justify-items-center mx-auto pt-2 md:pt-3 mb-10 max-w-6xl lg:max-w-none ml-4 mr-4 xl:ml-15 xl:mr-15">
+      {/* name1 */}
+      <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm   h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
     </section>
-  </section>
-     
-    <section className="mb-4 flex-1 ml-2 mr-2 h-[197px] md:h-[326px]">
-    <section className=" relative overflow-hidden max-h-full">
-      <Image src="/maleOutline.png" width="295"  height="326"  alt="other image"/>
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className=" text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small mx-2 text-white">Role</p>
-      </section>
     </section>
-  </section>
-  
-
-  <section className="mb-4 flex-1 ml-2 mr-2
-    h-[197px] md:h-[326px]">
-    <section className="relative 
-      overflow-hidden max-h-full">
-      <Image src="/femaleOutline2.png" width="295" height="326" alt="other image" />
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2 ">
-        <p className="text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small text-white mx-2">Role</p>
-      </section>
     </section>
-  </section>
-   
-  <section className="mb-4 flex-1 ml-2 mr-2
-    h-[197px] md:h-[326px]">
-    <section className="relative 
-      overflow-hidden max-h-full">
-      <Image src="/femaleOutline2.png" width="295" height="326" alt="other image" />
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2 ">
-        <p className="text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small text-white mx-2">Role</p>
-      </section>
+    {/* name2 */}
+      
+    <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm  min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
     </section>
-  </section>
-   
-  <section className="mb-4 flex-1 ml-2 mr-2 h-[197px] md:h-[326px]">
-    <section className=" relative overflow-hidden max-h-full">
-      <Image src="/maleOutline.png" width="295"  height="326"  alt="other image"/>
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className=" text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small mx-2 text-white">Role</p>
-      </section>
     </section>
-  </section>
-  
-
-  <section className="mb-4 flex-1 ml-2 mr-2 h-[197px] md:h-[326px]">
-    <section className=" relative overflow-hidden max-h-full">
-      <Image src="/maleOutline.png" width="295"  height="326"  alt="other image"/>
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className=" text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small mx-2 text-white">Role</p>
-      </section>
     </section>
-  </section>
-  
-
-  <section className="mb-4 flex-1 ml-2 mr-2 h-[197px] md:h-[326px]">
-    <section className=" relative overflow-hidden max-h-full">
-      <Image src="/maleOutline.png" width="295"  height="326"  alt="other image"/>
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className=" text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small mx-2 text-white">Role</p>
-      </section>
+    {/* name1 */}
+    <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
     </section>
-  </section>
-  
-
-
-  <section className="mb-4 flex-1 ml-2 mr-2
-    h-[197px] md:h-[326px]">
-    <section className="relative 
-      overflow-hidden max-h-full">
-      <Image src="/femaleOutline2.png" width="295" height="326" alt="other image" />
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2 ">
-        <p className="text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small text-white mx-2">Role</p>
-      </section>
     </section>
-  </section>
-
-  <section className="mb-4 flex-1 ml-2 mr-2 h-[197px] md:h-[326px]">
-    <section className=" relative overflow-hidden max-h-full">
-      <Image src="/maleOutline.png" width="295"  height="326"  alt="other image"/>
-      <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className=" text-base md:text-[20px] text-white mx-2">Name</p>
-        <p className="text-small mx-2 text-white">Role</p>
-      </section>
     </section>
-  </section>
-  
+    {/* name2 */}
+      
+    <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px]  xl:max-w-sm min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
+    </section>
+    </section>
+    </section>
+    {/* name1 */}
+    <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
+    </section>
+    </section>
+    </section>
+      {/* name1 */}
+      <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
+    </section>
+    </section>
+    </section>
+    {/* name2 */}
+      
+    <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px]  xl:max-w-sm min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
+    </section>
+    </section>
+    </section>
+     {/* name1 */}
+     <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
+    </section>
+    </section>
+    </section>
+   {/* name2 */}
+      
+   <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px]  xl:max-w-sm min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
+    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
+    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
+        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
+    </section>
+    </section>
+    </section>
   
 </section>
 
