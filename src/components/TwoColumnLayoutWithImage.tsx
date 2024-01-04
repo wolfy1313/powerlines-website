eb114b692f25927f0e26f4f7057e8d330053b70c
Shondriane <shondriane.wise@yahoo.com>
2024-01-03 18:42:44 -0800

    created reusable component for the 2-columns and image

---
+++ src/components/TwoColumnLayoutWithImage.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Josefin_Sans,Playfair_Display } from 'next/font/google'
+import Image from 'next/image'
+const josefinSans = Josefin_Sans({ subsets: ['latin'] })
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
+
+interface TwoColumnLayoutWithImageProps{
+    header: string;
+    body: string;
+    body2?: string;
+    imageSrc: string;
+    alt:string;
+    width: number|`${number}`;
+    height: number|`${number}`;
+}
+
+const TwoColumnLayoutWithImage:React.FC<TwoColumnLayoutWithImageProps>=({header,imageSrc,body,body2, width, height,alt})=> {
+  return (
+    <section className="min-w-full max-w-full mx-auto">
+     <section className="flex flex-wrap bg-white p-5 md:p-20  mb-[90px] md:mb-[150px] mt-20 md:mt-0"> 
+    {/* header visible on small screens */}
+    <section className="w-full order-1 md:order-1  md:hidden">
+    <h2 className={`text-xl md:text-3xl font-bold mb-4 text-darker-yellow ${josefinSans.className}`}>{header}</h2>
+  </section>
+{/* 
+Text Column  */}
+ <section className="w-full md:w-1/2 pt-12 pl-0  md:pl-12flex items-center justify-center h-auto order-3 md:order-2 ">
+ <section className=" mt-0 md:mt-12 lg:mt-20">
+    {/* Header hidden on small screens, shown on medium and larger screens */}
+   <h2 className={`text-xl md:text-3xl font-bold mb-4 lg:mb-20 text-darker-yellow hidden md:block ${josefinSans.className}`}>{header}</h2> 
+<p className={`text-base md:text-xl font-normal leading-9 mb-8  ${playfairDisplay.className}`}>{body}</p> <p className={`text-base md:text-sm font-normal leading-9 ${playfairDisplay.className}`}>{body2}</p>
+
+   </section>
+  </section>
+   {/* Image Column  */}
+   <section className="w-full  pt-12 pl-0 md:w-1/2 md:pl-12 order-2 md:order-3  mb-[50px] md:mb-[120px] " >
+    <section className=" w-full sm:min-w-*[186px] h-auto md:max-w-*[555px] md:max-h-*[356px]  mt-0 md:mt-20 flex justify-center">
+    <Image width={width} height={height}  src={imageSrc} alt={alt} />
+    </section>
+  </section>
+ </section>
+ 
+    </section>
+  )
+}
+
+export default TwoColumnLayoutWithImage
--- src/pages/CommunityEngagement.tsx
+++ src/pages/CommunityEngagement.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import TOCHeaderComponent from '@/components/TOCHeaderComponent'
+import TwoColumnLayoutWithImage from '@/components/TwoColumnLayoutWithImage'
 
 function CommunityEngagement() {
   return (
     <section>
         <TOCHeaderComponent header="THEORY OF CHANGE" pageTitle='COMMUNITY ENGAGEMENT' />
+        <TwoColumnLayoutWithImage header="WHY IS COMMUNITY ENGAGEMENT IMPORTANT AND WHAT CHALLENGES ARE WE FACING?" body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." width="555" height="356" imageSrc='/monumentPlaceholder.png' alt="monument place holder"/>
     </section>
   )
 }
