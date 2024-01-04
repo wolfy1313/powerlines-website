0dda80b587207642b1ff2efc7adf63797255dc7e
Shondriane <shondriane.wise@yahoo.com>
2024-01-03 16:57:00 -0800

    adding the TOCHeader component to Community Engagement Page

---
+++ src/pages/CommunityEngagement.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TOCHeaderComponent from '@/components/TOCHeaderComponent'
+
+function CommunityEngagement() {
+  return (
+    <section>
+        <TOCHeaderComponent header="THEORY OF CHANGE" pageTitle='COMMUNITY ENGAGEMENT' />
+    </section>
+  )
+}
+
+export default CommunityEngagement
