49cc42c0248231f89983b18291d7998b020ec315
jeremyHarrell <hillvulf@gmail.com>
2024-01-04 22:10:51 -0600

    progress on TOCOutcomesComponent.tsx | Added to PoliticalStrategy.tsx page

--- src/components/TOCOutcomesComponent.tsx
+++ src/components/TOCOutcomesComponent.tsx
@@ -8,16 +8,32 @@ const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 interface TOCOutcomesComponentProps {
   header: string;
   content: string;
-  imageLabel: string;
 }
 
 const TOCOutcomesComponent: React.FC<TOCOutcomesComponentProps> = ({ header, content, imageLabel }) => {
   return (
-    <div className={`min-w-screen max-w-screen ${josefinSans.className}`}>
-      <h1>{header}</h1>
-      <h3>{content}</h3>
-      <section>
-        <Image src="./OutcomeMailImage.svg" alt="mailbox icon" />
+    <div className={`min-w-screen max-w-screen`}>
+      <h1 className={`mb-4 text-main-yellow text-xl ${josefinSans.className}`}>{header}</h1>
+      <h3 className={`mb-8 ${playfairDisplay.className} text-sm`}>{content}</h3>
+      <section className="flex flex-row justify-around mx-20">
+        <div className=" flex flex-col items-center">
+          <div className=" bg-main-yellow rounded-full p-4 pb-5" >
+            <Image src="./OutcomeMailImage.svg" alt="mailbox icon" width={32} height={32} className=" bg-main-yellow" />
+          </div>
+          <h2 className="mt-2 font-semibold">Outcome 1</h2>
+        </div>
+        <div className=" flex flex-col items-center">
+          <div className=" bg-main-yellow rounded-full p-4 pb-5" >
+            <Image src="./OutcomeMailImage.svg" alt="mailbox icon" width={32} height={32} className=" bg-main-yellow" />
+          </div>
+          <h2 className="mt-2 font-semibold">Outcome 2</h2>
+        </div>
+        <div className=" flex flex-col items-center">
+          <div className=" bg-main-yellow rounded-full p-4 pb-5" >
+            <Image src="./OutcomeMailImage.svg" alt="mailbox icon" width={32} height={32} className=" bg-main-yellow" />
+          </div>
+          <h2 className="mt-2 font-semibold">Outcome 3</h2>
+        </div>
       </section>
     </div>
   )
