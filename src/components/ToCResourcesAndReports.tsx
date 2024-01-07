c0323f09392fb23b95ed84af78debc832aa3ec8e
Shondriane <shondriane.wise@yahoo.com>
2024-01-07 11:27:06 -0800

    create resources and reports component

---
+++ src/components/ToCResourcesAndReports.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Josefin_Sans, Playfair_Display } from "next/font/google";
+import Image from 'next/image'
+
+const ToCResourcesAndReports = () => {
+  return (
+    <div>ToCResourcesAndReports</div>
+  )
+}
+
+export default ToCResourcesAndReports
