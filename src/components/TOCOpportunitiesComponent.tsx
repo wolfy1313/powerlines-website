62a48094056af89a9150035efb12a27fea65861e
jeremyHarrell <hillvulf@gmail.com>
2024-01-08 11:02:33 -0600

    create TOCOpportunitiesComponent.tsx -- styling. Add component to PoliticalStrategy page.

--- src/components/TOCOpportunitiesComponent.tsx
+++ src/components/TOCOpportunitiesComponent.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Josefin_Sans, Playfair_Display } from "next/font/google";
 
-const TOCOpportunitiesComponent = () => {
+const josefinSans = Josefin_Sans({ subsets: ['latin'] })
+const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
+
+interface TOCOpportunitiesComponentProps {
+  header: string;
+  content: string;
+}
+
+
+const TOCOpportunitiesComponent: React.FC<TOCOpportunitiesComponentProps> = ({ header, content }) => {
   return (
-    <div>TOCOpportunitiesComponent</div>
+    <section className='min-w-screen max-w-screen md:px-20 md:pb-5 md:pt-0'>
+      <h1 className={`mb-4 text-main-yellow text-xl ${josefinSans.className}`}>{header}</h1>
+      <h3 className={`mb-8 ${playfairDisplay.className} text-sm`}>{content}</h3>
+    </section>
   )
 }
 
