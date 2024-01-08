ea1c043d36456fa9baa0a66f26b20c35bed507d3
jeremyHarrell <hillvulf@gmail.com>
2024-01-08 14:36:24 -0600

    update font in TrunctableText.tsx update spacing in ToCResourcesAndReports.tsx

--- src/components/ToCResourcesAndReports.tsx
+++ src/components/ToCResourcesAndReports.tsx
@@ -1,18 +1,18 @@
 
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Josefin_Sans, Playfair_Display } from "next/font/google";
 import Image from 'next/image'
 import TrunctableText from './TrunctableText';
 import Link from 'next/link';
 
-import { TOCResourcesAndReportsProps,ResourcesAndReportsCardsProps } from '@/types/global';
+import { TOCResourcesAndReportsProps, ResourcesAndReportsCardsProps } from '@/types/global';
 const josefinSans = Josefin_Sans({ subsets: ['latin'] })
 const playfairDisplay = Playfair_Display({ subsets: ['latin'] })
 
 
 
-const ToCResourcesAndReports:React.FC<TOCResourcesAndReportsProps> = ({ ResourcesAndReportsCards }) => {
+const ToCResourcesAndReports: React.FC<TOCResourcesAndReportsProps> = ({ ResourcesAndReportsCards }) => {
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
@@ -20,23 +20,23 @@ const ToCResourcesAndReports:React.FC<TOCResourcesAndReportsProps> = ({ Resource
         setIsClient(true);
     }, []);
 
-  return (
-    <div className="min-w-full max-w-full mx-auto">
+    return (
+        <div className="min-w-full max-w-full mx-auto">
             <section className="p-10 md:p-20 md:pt-0">
                 <h1 className={`mb-4 text-darker-yellow text-2xl md:text-3xl font-semibold leading-normal ${josefinSans.className}`}>Resource and Reports</h1>
                 <section className="grid grid-cols-1  gap-4 md:gap-10">
-                    {ResourcesAndReportsCards.map((ResourcesAndReportsCard:ResourcesAndReportsCardsProps) => (
-                        <div className="w-full flex flex-col md:flex-row gap-20">
-                            <section className="relative h-330 w-520">
+                    {ResourcesAndReportsCards.map((ResourcesAndReportsCard: ResourcesAndReportsCardsProps) => (
+                        <div key={ResourcesAndReportsCard.caption} className="flex flex-col md:flex-row gap-20">
+                            <section className="h-330 w-520">
                                 <Image width='505' height='330' src={ResourcesAndReportsCard.image} alt={ResourcesAndReportsCard.alt} />
                             </section>
-                            <section className="relative h-330 w-520">
+                            <section className="h-330 w-520">
                                 <h2 className={`mb-4 text-darker-yellow text-sm md:text-base font-normal leading-8 ${josefinSans.className}`}>{ResourcesAndReportsCard.caption}</h2>
                                 <h1 className={`mb-4 text-black  text-xl md:text-2xl font-semibold leading-8 ${josefinSans.className}`}>{ResourcesAndReportsCard.subHeading}</h1>
                                 {isClient && (
                                     <TrunctableText text={ResourcesAndReportsCard.body} maxLength={150} />
                                 )}
-                                <h6 className={`mb-6 text-black text-sm md:text-base font-normal leading-8 ${josefinSans.className}`}>{ResourcesAndReportsCard.cta}</h6>
+                                <h6 className={`mb-6 text-black text-sm md:text-base tracking-tighter font-normal leading-8 ${josefinSans.className}`}>{ResourcesAndReportsCard.cta}</h6>
                                 <div className="flex">
                                     <section className="relative h-100 w-100 flex flex-row items-center mr-4">
                                         <Image width={20} height={20} src={ResourcesAndReportsCard.downloadIcon} alt="Download Icon" />
@@ -53,7 +53,7 @@ const ToCResourcesAndReports:React.FC<TOCResourcesAndReportsProps> = ({ Resource
                 </section>
             </section>
         </div>
-  )
+    )
 }
 
 export default ToCResourcesAndReports
