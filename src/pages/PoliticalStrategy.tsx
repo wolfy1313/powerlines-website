f518b169cec94d6a96a94a5c01040aeb82f6266e
Stephan D Ramalho <sramalho@fordham.edu>
2024-01-09 15:56:05 -0500

    Configure page for TocBodyList
    Create content for component
    Pass content as props

--- src/pages/PoliticalStrategy.tsx
+++ src/pages/PoliticalStrategy.tsx
@@ -3,7 +3,11 @@ import TOCHeaderComponent from '@/components/TOCHeaderComponent'
 import TwoColumnLayoutWithImage from '@/components/TwoColumnLayoutWithImage'
 import TOCOutcomesComponent from '@/components/TOCOutcomesComponent'
 
-import { OutcomeCardProps, TOCOutcomesComponentProps } from '@/types/global'
+import {
+  OutcomeCardProps,
+  TOCOutcomesComponentProps,
+  bulletPoint
+} from '@/types/global'
 import TOCOpportunitiesComponent from '@/components/TOCOpportunitiesComponent'
 import TOCBodyList from '@/components/TOCBodyList'
 const OutcomeCardsData: OutcomeCardProps[] = [
@@ -30,6 +34,25 @@ const OutcomeCardsData: OutcomeCardProps[] = [
   }
 ]
 
+const bulletPoints: bulletPoint[] = [
+  [
+    'Increased Funding',
+    'Allocate more financial resources to PUC races and fundraising efforts, which is critical to catalyzing positive change that yields favorable climate outcomes.'
+  ],
+  [
+    'Advocacy with Governors',
+    'Engage with governors, advocating for the appointment of climate and equity champions to PUCs through organized coalitions.'
+  ],
+  [
+    'Candidate Recruitment',
+    'Actively recruit qualified candidates for PUC races, ensuring a strong pool of individuals committed to the cause.'
+  ],
+  [
+    'Commissioner Training',
+    'Facilitate ongoing training for PUC commissioners and staff, equipping them with resources and effective strategies.'
+  ]
+]
+
 function PoliticalStrategy() {
   return (
     <div>
@@ -49,7 +72,12 @@ function PoliticalStrategy() {
         header="OPPORTUNITIES"
         content="We urgently need more PUC commissioners who champion climate and equity. To achieve this, campaigns can influence governors and state legislatures to appoint such commissioners and encourage like-minded candidates to run and win PUC elections. These elections offer high returns-on-investment for climate and equity, with candidates raising an average of only $[] per election over the last decade. Closing the voter dropoff gap between Democratic and Republican PUC candidates can greatly enhance the competitiveness of climate and equity champions. This strategy has proven successful, as seen in North Carolina and the 2022 Louisiana PSC race where climate justice priorities were advanced."
       />
-      <TOCBodyList header="THEORY OF CHANGE" />
+      <TOCBodyList
+        header="THEORY OF CHANGE"
+        subheader="Through data-driven research, PowerLines will identify key appointments and elections with the greatest impact. PowerLines will then work with electoral and advocacy partners to push for the appointment and election of climate and equity champions to PUCs."
+        bulletPoints={bulletPoints}
+        footer="Our goal through this strategy works because putting more money into PUC campaigns can help improve climate outcomes. PUC races don't cost much compared to the impact they can have, making it a smart investment. Not many people are paying attention to these issues right now, so raising awareness about important things like water quality and linking them to everyday concerns like energy bills can get more people involved. This strategy focuses on races where success is more probable, especially those that can change commissions from red to blue."
+      />
       <TOCOutcomesComponent
         header="OUTCOMES"
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
