7c596a9d2463d422658bc17f672617d95a780597
Stephan D Ramalho <sramalho@fordham.edu>
2024-01-23 18:22:38 -0500

    Replace model legislation section with ToCCard component

--- src/components/TheoryOfChange.tsx
+++ src/components/TheoryOfChange.tsx
@@ -43,49 +43,12 @@ function TheoryOfChange() {
               checklist={['Appoints Champions', 'Elects Champions']}
             />
 
-            <section className="bg-white shadow-custom rounded-3xl overflow-hidden mx-2 my-5 flex flex-col h-428 w-400 text-center md:flex-grow md:flex-shrink md:basis-1/3">
-              <section className="mt-5 h-113 w-140">
-                <Image
-                  src="/model-legislation.svg"
-                  alt="model legislation icon"
-                  width={161}
-                  height={170}
-                  className="color-yellow md:mt-3 mx-auto"
-                />
-              </section>
-              <section className="px-6 py-4 flex-grow">
-                <section
-                  className={`font-bold text-xl text-center mb-2 ${josefinSans.className}`}
-                >
-                  Model Legislation
-                </section>
-                <ul className="list-inside text-black-400 text-base p-5 space-y-4 pl-20 md:pl-10 lg:pl-24">
-                  <li className="flex flex-row">
-                    <Image src="/checkmark.svg" height={20} width={20} alt="" />{' '}
-                    Passes Legislation
-                  </li>
-                  <li className="flex flex-row">
-                    <Image src="/checkmark.svg" height={20} width={20} alt="" />{' '}
-                    Lorem Ipsum
-                  </li>
-                </ul>
-              </section>
-              <section className="text-center mb-5">
-                <Link
-                  href="#"
-                  className={`inline-flex items-center my-0 mx-0 px-6 py-3 text-sm font-bold text-center text-[#E2B103] bg-main-white border-2 border-main-yellow rounded-2xl ${josefinSans.className}`}
-                >
-                  Learn More
-                  <Image
-                    src="/arrow-right2.svg"
-                    alt=""
-                    height={5}
-                    width={28}
-                    className="ml-2"
-                  />
-                </Link>
-              </section>
-            </section>
+            <ToCCard
+              header="Model Legislation"
+              imageSrc="/model-legislation.svg"
+              href="/model-legislation"
+              checklist={['Passes Legislation', 'Lorem Ipsum']}
+            />
 
             <section className="bg-white shadow-custom rounded-3xl overflow-hidden mx-2 my-5 flex flex-col h-428 w-400 text-center md:flex-grow md:flex-shrink md:basis-1/3">
               <section className="mt-5 h-113 w-140">
