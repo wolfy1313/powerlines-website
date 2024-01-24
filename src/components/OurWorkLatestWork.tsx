d4cf12ad3a263b4307f97970524f9ba631255899
jeremyHarrell <hillvulf@gmail.com>
2024-01-24 11:11:56 -0600

    adjust borders on Our Latest Work dropdown

--- src/components/OurWorkLatestWork.tsx
+++ src/components/OurWorkLatestWork.tsx
@@ -40,7 +40,7 @@ const Section: React.FC<SectionProps> = ({ title, content, download, link }) => 
   };
 
   return (
-    <section className={`${josefinSans.className}  grid grid-cols-1 md:place-content-between mx-6  border-t-2 border-b-2 font-medium overflow-hidden transition-[max-height] duration-500 ease-in-out ${isDropdownVisible ? "md:max-h-96" : "max-h-[4.7rem] md:max-h-20"}`} >
+    <section className={`${josefinSans.className}  grid grid-cols-1 md:place-content-between mx-6  border-t-4  font-medium overflow-hidden transition-[max-height] duration-500 ease-in-out ${isDropdownVisible ? "md:max-h-96" : "max-h-[4.7rem] md:max-h-20"}`} >
       <div className='flex justify-between items-center'>
         <div className=' py-5 text-lg sm:text-xl md:text-2xl col-span-2 max-w-2xl cursor-pointer' onClick={toggleDropdown}>{title}</div>
         <button className={`grid place-content-end cursor-pointer ${isDropdownVisible ? "pb-1" : "pb-5"} pr-8 col-start-3`} onClick={toggleDropdown}>
@@ -58,8 +58,8 @@ const Section: React.FC<SectionProps> = ({ title, content, download, link }) => 
 
 function OurWorkLatestWork() {
   return (
-    <div className='flex flex-col border-b-4'>
-      <div className='pt-16 pb-10 border-b-4 mx-6'>
+    <div className='flex flex-col'>
+      <div className='pt-16 pb-10 '>
         <h1 className={`${josefinSans.className} text-darker-yellow text-center text-3xl md:text-5xl font-bold`} >
           OUR LATEST WORK
         </h1>
