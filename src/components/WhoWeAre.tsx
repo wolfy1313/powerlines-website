2417f0171a4dbf2922ba72d62634a47fe49d8dae
Stephan D Ramalho <sramalho@fordham.edu>
2024-01-31 13:15:57 -0500

    Align text to the left

--- src/components/WhoWeAre.tsx
+++ src/components/WhoWeAre.tsx
@@ -8,28 +8,28 @@ const WhoWeAre = React.forwardRef<HTMLDivElement>((_props, ref) => {
   return (
     <section
       ref={ref}
-      className="min-w-screen max-w-screen bg-no-repeat bg-[url('/WhoWeAreBG.svg')] bg-cover text-center"
+      className="min-w-screen max-w-screen bg-no-repeat bg-[url('/WhoWeAreBG.svg')] bg-cover"
     >
-      <section className="flex flex-row justify-center">
+      <section className="flex flex-row justify-start mx-20">
         <section className="font-bold text-2xl md:text-3xl mt-8 md:mt-16">
           <h2 className={`${cairo.className}`}>WHO WE ARE</h2>
         </section>
       </section>
 
-      <section className="flex flex-row justify-center">
+      <section className="flex flex-row justify-start mx-20">
         <h1
-          className={`text-center font-bold text-4xl leading-relaxed ml-auto mr-auto md:text-6xl mt-8 md:mt-16 ${cairo.className}`}
+          className={`font-bold text-4xl leading-relaxed md:text-6xl mt-8 md:mt-16 ${cairo.className}`}
         >
           OUR FOCUS IS BRINGING <br />
           <span className="text-main-one">POWER</span> TO THE PUBLIC
         </h1>
       </section>
 
-      <section className="flex flex-row flex-1 my-auto justify-center">
+      <section className="flex flex-row flex-1 my-auto justify-start mx-20">
         <section
           className={`${robotoSlab.className} text-sm my-auto px-2  mt-10 md:text-xl md:tracking-wider md:min-w-fit  min-w-screen leading-10`}
         >
-          <h3 className={`max-w-screen md:min-w-screen text-center`}>
+          <h3 className={`max-w-xl md:min-w-screen`}>
             PowerLines is a non-governmental <br className="block md:hidden" />{' '}
             organization that is passionate about{' '}
             <br className="block md:hidden" /> restructuring the{' '}
