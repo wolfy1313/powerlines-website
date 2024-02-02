16d51e03771e2998d8764ed6ab10d76022a22a82
jeremyHarrell <hillvulf@gmail.com>
2024-02-02 15:40:31 -0600

    add new background to OurWorkPage.tsx

--- src/pages/OurWorkPage.tsx
+++ src/pages/OurWorkPage.tsx
@@ -18,39 +18,41 @@ function OurWorkPage() {
       </section>
 
       {/* {Our Problem Section} */}
-      <section className=' flex flex-col-reverse px-3 py-3 md:flex md:flex-row max-w-screen md:px-10 md:py-10'>
-        <section className='flex md:px-7 px-3 items-center ' >
-          <OurWorkProblem />
+      <section className={`bg-no-repeat bg-[url('/OurWorkBG_NEW.png')] bg-cover`}>
+        <section className=' flex flex-col-reverse px-3 py-3 md:flex md:flex-row max-w-screen md:px-10 md:py-10'>
+          <section className='flex md:px-7 px-3 items-center ' >
+            <OurWorkProblem />
+          </section>
+          <section className='px-4'>
+            <OurWorkImage />
+          </section>
+          <section className='ml-1 md:hidden'>
+            <OurWorkProblemHeader />
+          </section>
         </section>
-        <section className='px-4'>
-          <OurWorkImage />
-        </section>
-        <section className='ml-1 md:hidden'>
-          <OurWorkProblemHeader />
-        </section>
-      </section>
 
-      {/* {Our Solution Section} */}
-      <section className='md:flex max-w-screen px-3 py-3 md:px-10 md:py-10 bg-light-yellow'>
-        <section className='ml-1 md:hidden'>
-          <OurWorkSolutionHeader />
+        {/* {Our Solution Section} */}
+        <section className='md:flex max-w-screen px-3 py-3 md:px-10 md:py-10 bg-light-yellow'>
+          <section className='ml-1 md:hidden'>
+            <OurWorkSolutionHeader />
+          </section>
+          <section className='px-4 pt-2'>
+            <OurWorkSolutionImage />
+          </section>
+          <section className='flex md:px-7 px-3 items-center' >
+            <OurWorkSolution />
+          </section>
         </section>
-        <section className='px-4 pt-2'>
-          <OurWorkSolutionImage />
-        </section>
-        <section className='flex md:px-7 px-3 items-center' >
-          <OurWorkSolution />
-        </section>
-      </section>
 
-      {/* {Theory of Change Section} */}
-      <section className="bg-no-repeat bg-[url('/OurWorkToCBG.svg')] bg-cover">
-        <TheoryOfChange />
-      </section>
+        {/* {Theory of Change Section} */}
+        <section className="bg-no-repeat bg-[url('/OurWorkToCBG.svg')] bg-cover">
+          <TheoryOfChange />
+        </section>
 
-      {/* {Latest Work Section} */}
-      <section className='mx-12 md:mx-12 border-b-4'>
-        <OurWorkLatestWork />
+        {/* {Latest Work Section} */}
+        <section className='mx-12 md:mx-12 border-b-4'>
+          <OurWorkLatestWork />
+        </section>
       </section>
 
 
