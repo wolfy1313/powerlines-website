c33536751ac68ecdbb8c00276512e77fc9fa6241
Brian Lara <Briandlara1@gmail.com>
2024-02-05 21:14:09 -0500

    made Our Team section resizable, and updated style

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 function OurTeam() {
   return (
-    <section className="">
+    <section className="w-3/4 m-auto">
    
       {/* Start of Our Team Section */}
       <section className="flex flex-col 
@@ -19,96 +19,114 @@ function OurTeam() {
    
 
     
-    <section className="grid grid-cols-2  md:grid-cols-3 gap-2 justify-items-center mx-auto pt-2 md:pt-3 mb-10 max-w-6xl lg:max-w-none ml-4 mr-4 xl:ml-15 xl:mr-15">
-      {/* name1 */}
-      <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm   h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-    {/* name2 */}
-      
-    <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm  min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-      {/* name1 */}
-      <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm   h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-   {/* name2 */}
-      
-   <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm  min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-  {/* name1 */}
-  <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm   h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-    {/* name2 */}
-      
-   <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm  min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-     {/* name1 */}
-  <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm   h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-     {/* name2 */}
-  
-   <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm  min-h-[197px] sm:h-auto md:h-[352px] overflow-hidden bg-yellow-100 bg-opacity-50 sm:mx-2">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/femaleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-    {/* name1 */}
-  <section className=" flex-1 mb-4 w-full sm:min-w-[165px]  md:max-w-[295px] xl:max-w-sm   h-auto sm:min-h-[197px] md:h-[352px] overflow-hidden bg-[#B08B1A26]">
-    <section className="relative h-full bg-no-repeat bg-contain bg-center bg-[url('/maleTransparent.png')]">
-    <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
-        <p className={`${josefinSans.className} text-base md:text-[20px]  text-white mx-2 font-bold leading-normal` }>Name</p>
-        <p className={`${playfairDisplay.className} text-small  text-white mx-2 font-normal leading-normal` }>Role</p>
-    </section>
-    </section>
-    </section>
-    
-</section>
+      <section className="grid grid-cols-2  md:grid-cols-3 gap-6 justify-items-center mx-auto max-w-6xl lg:max-w-none ">
+        
+        {/* name1 */}
+        <div className=" max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#B08B1A] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/maleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-cover" />
+              <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </section>
+          </div>
+        </div>
 
-    </section>
+   
+        {/* Name 2 */}
+        <div className=" max-w-[295px] md:max-w-none w-full overflow-hidden
+        bg-[#FFFEE8] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/femaleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-fit" />
+              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name 1</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </div>
+          </div>
+        </div>
+
+
+
+        {/* name3 */}
+        <div className=" max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#B08B1A] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/maleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-cover" />
+              <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </section>
+          </div>
+        </div>
+    
+        {/* name2 */}   
+        <div className="max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#FFFEE8] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/femaleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-fit" />
+              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name 1</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </div>
+          </div>
+        </div>
+    
+        {/* name1 */}
+        <div className="max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#B08B1A] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/maleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-cover" />
+              <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </section>
+          </div>
+        </div>
+      
+        {/* name2 */}   
+        <div className="max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#FFFEE8] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/femaleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-fit" />
+              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name 1</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </div>
+          </div>
+        </div>
+      
+        {/* name1 */}
+        <div className="max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#B08B1A] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/maleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-cover" />
+              <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </section>
+          </div>
+        </div>
+      
+        {/* name2 */}
+        <div className="max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#FFFEE8] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/femaleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-fit" />
+              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name 1</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </div>
+          </div>
+        </div>
+      
+        {/* name1 */}
+        <div className="max-w-[295px] md:max-w-none w-full overflow-hidden bg-[#B08B1A] bg-opacity-50">
+          <div className="relative pb-[118.64%]"> {/* Aspect ratio padding-bottom hack */}
+              <img src="/maleTransparent.png" alt="Profile" className="absolute top-0 left-0 w-full h-full object-cover" />
+              <section className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-40 p-1 md:p-2">
+                <p className="text-sm sm:text-base lg:text-lg text-white mx-2 font-bold leading-normal">Name</p>
+                <p className="text-xs sm:text-sm lg:text-base text-white mx-2 font-normal leading-normal">Role</p>
+              </section>
+          </div>
+        </div>
+      
+  </section>
+
+      </section>
  
   )
 }
