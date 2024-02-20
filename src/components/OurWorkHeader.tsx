b968f874a9967c624490750bdda4e287904eca67
jeremyHarrell <hillvulf@gmail.com>
2024-02-20 16:06:07 -0600

    add new background to OurWorkHeader.tsx

--- src/components/OurWorkHeader.tsx
+++ src/components/OurWorkHeader.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function OurWorkHeader() {
   return (
     <section className='min-w-screen max-w-screen'>
-      <section className=" flex bg-no-repeat bg-[url('/HeaderPowerLinesCircle.svg')] h-64 bg-right">
+      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-64 bg-center">
         <h1 className={`flex items-center font-bold align-middle p-16 text-2xl md:text-5xl font-main`}>OUR WORK</h1>
       </section>
     </section>
