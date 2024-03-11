f2b0d0874eecc4c72a10be8e1bfc1c36635fe1c7
jeremyHarrell <hillvulf@gmail.com>
2024-03-11 13:02:06 -0500

    update vertical alignment of OurLatestWork

--- src/components/OurWorkLatestWork.tsx
+++ src/components/OurWorkLatestWork.tsx
@@ -40,7 +40,7 @@ const Section: React.FC<SectionProps> = ({ title, content, download, link }) => 
   };
 
   return (
-    <section className={`${cairo.className}  grid grid-cols-1 md:place-content-between mx-6  border-t-4  font-medium overflow-hidden transition-[max-height] duration-500 ease-in-out ${isDropdownVisible ? "md:max-h-96" : "max-h-[5.75rem] md:max-h-24"}`} >
+    <section className={`${cairo.className}  grid grid-cols-1 md:place-content-between mx-6 border-t-4 font-medium overflow-hidden transition-[max-height] duration-500 ease-in-out ${isDropdownVisible ? "md:max-h-96" : "max-h-[5.75rem] md:max-h-24"}`} >
       <div className='flex justify-between items-center pt-2'>
         <div className=' py-5 text-lg sm:text-xl md:text-2xl col-span-2 max-w-2xl cursor-pointer' onClick={toggleDropdown}>{title}</div>
         <button className={`grid place-content-end cursor-pointer ${isDropdownVisible ? "pb-1" : "pb-5"} pr-8 col-start-3`} onClick={toggleDropdown}>
@@ -50,7 +50,7 @@ const Section: React.FC<SectionProps> = ({ title, content, download, link }) => 
       <div className={`col-span-3 pr-8 pb-8 overflow-hidden transition-[max-height] duration-500 ease-in-out ${isDropdownVisible ? "max-h-72" : "max-h-0"}`}>
         <div className={`${robotoSlab.className}  text-sm font-light md:text-xl pr-2 pl-2 md:pr-20 md:pl-20 pt-4 pb-10`}>{content}</div>
         <div className={`${cairo.className} font-bold pl-2 md:pl-20`}>{download}</div>
-        <div className={`${cairo.className} flex underline font-semibold pl-2 md:pl-20 text-main-one`}>{link}</div>
+        <div className={`${cairo.className} flex underline font-semibold pl-2 md:py-4 md:pl-20 text-[#264D70] md:text-xl`}>{link}</div>
       </div>
     </section>
   );
@@ -69,8 +69,8 @@ function OurWorkLatestWork() {
         title='Lorem Ipsum dolor sit amet, consectetur adipiscing elit '
         content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         download='Download'
-        link={<Link href={`./about-us`} className='flex'>
-          <Image src="/simcarddownload.svg" width={20} height={20} alt="download image" />
+        link={<Link href={`./about-us`} className='flex gap-3'>
+          <Image src="/newSimCardDownload.png" width={20} height={20} alt="download image" className='' />
           Full White Paper
         </Link>}
       />
@@ -79,8 +79,8 @@ function OurWorkLatestWork() {
         title='Lorem Ipsum dolor sit amet, consectetur adipiscing elit '
         content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         download='Download'
-        link={<Link href={`./about-us`} className='flex'>
-          <Image src="/simcarddownload.svg" width={20} height={20} alt="download image" />
+        link={<Link href={`./about-us`} className='flex gap-3'>
+          <Image src="/newSimCardDownload.png" width={20} height={20} alt="download image" className='' />
           Full White Paper
         </Link>}
       />
@@ -89,8 +89,8 @@ function OurWorkLatestWork() {
         title='Lorem Ipsum dolor sit amet, consectetur adipiscing elit '
         content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         download='Download'
-        link={<Link href={`./about-us`} className='flex'>
-          <Image src="/simcarddownload.svg" width={20} height={20} alt="download image" />
+        link={<Link href={`./about-us`} className='flex gap-3'>
+          <Image src="/newSimCardDownload.png" width={20} height={20} alt="download image" className='' />
           Full White Paper
         </Link>}
       />
