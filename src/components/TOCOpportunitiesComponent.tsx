eeb7cb6e55fcd07e722a344768c43097ccf769b3
jeremyHarrell <hillvulf@gmail.com>
2024-03-25 14:03:42 -0500

    change opportunities section to match figma on ModelLegislation.tsx

--- src/components/TOCOpportunitiesComponent.tsx
+++ src/components/TOCOpportunitiesComponent.tsx
@@ -13,8 +13,8 @@ interface TOCOpportunitiesComponentProps {
 const TOCOpportunitiesComponent: React.FC<TOCOpportunitiesComponentProps> = ({ header, content }) => {
   return (
     <section className='min-w-screen max-w-screen md:px-20 md:pb-5 md:pt-0'>
-      <h1 className={`mb-4 text-main-one text-xl ${cairo.className}`}>{header}</h1>
-      <h3 className={`mb-8 ${robotoSlab.className} text-sm`}>{content}</h3>
+      <h1 className={`mb-4 text-[#B6163E] text-4xl ${cairo.className}`}>{header}</h1>
+      <h3 className={`mb-8 ${robotoSlab.className} text-xl`}>{content}</h3>
     </section>
   )
 }
