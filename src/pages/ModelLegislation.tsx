f7b0276608aee809148d4cb1f85b404b64fbd0a2
jeremyHarrell <hillvulf@gmail.com>
2024-03-25 15:06:23 -0500

    update ModelLegislation.tsx and TwoColumnLayoutWithImage component

--- src/pages/ModelLegislation.tsx
+++ src/pages/ModelLegislation.tsx
@@ -64,8 +64,8 @@ function ModelLegislation() {
         header="WHY IS MODEL LEGISLATION IMPORTANT AND WHAT CHALLENGES ARE WE FACING?"
         body="Public Utility Commissions (PUCs), responsible for the energy transition, face a shortage of progressive commissioners, either elected or appointed by Governors. About 80% of them are chosen by the state's governor or legislature, and 20% are elected. Utilities' influence on PUCs prioritizes fossil fuel interests, hindering clean energy and fairness. They lobby against climate and equity policies and sway elections in favor of utility-friendly candidates (Stokes 2020). Additionally, PUC commissioners lack diversity, worsening energy inequities for people of color and women. Powerlines seeks to break this cycle by supporting progressive candidates, raising voter awareness, and mitigating private utility influence."
         width="608"
-        height="385"
-        imageSrc="/monumentPlaceholder.png"
+        height="608"
+        imageSrc="/ModelLegislationImage.svg"
         alt="monument place holder"
       />
       <TOCOpportunitiesComponent
