b947170d638dadc178de9dfb0c0d576ccc1e8760
Kevin Castro <kcapupp@gmail.com>
2024-03-26 21:06:14 -0400

    Hero page change

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -10,14 +10,17 @@ interface HomeHeaderProps {
 }
 function HomeHeader({ scrollToWhoWeAre }: HomeHeaderProps) {
   return (
-    <section className="min-w-screen max-w-screen pb-[460px] bg-cover bg-contain bg-no-repeat bg-[url('/Windmill_BG.jpg')] text-white  md:bg-[url('/home-header.svg')] bg-[75%] md:h-[60rem] md:pb-32 lg:pb-0">
+    <section
+      className={`min-w-screen max-w-screen h-screen bg-cover bg-contain bg-no-repeat bg-[url('/Windmill_BG.jpg')] text-white md:bg-[url('/home-header.svg')] bg-[75%] md:pb-32 lg:pb-0`}
+    >
       {/* Text Container */}
       <section className="flex flex-row justify-center">
         {/* Title Text */}
-        <section className="flex-col font-bold tracking-wider text-3xl md:text-5xl mt-32 md:mt-48">
-          <h2 className={`flex flex-row ${cairo.className}`}>
-            <span className="px-3">Power</span>
-            to the Public
+        <section className="flex-col font-bold tracking-wider leading:[36px] mt-32 md:mt-48">
+          <h2
+            className={`flex flex-row ${cairo.className} text-[29px] md:text-[55px]`}
+          >
+            Power to the Public
           </h2>
         </section>
       </section>
@@ -25,17 +28,19 @@ function HomeHeader({ scrollToWhoWeAre }: HomeHeaderProps) {
       {/* Subtitle Container */}
       <section className="flex flex-row flex-1 justify-center min-w-screen my-auto text-center">
         <section
-          className={`flex flex-col ${robotoSlab.className} text-sm text-center my-auto  mt-6 md:text-xl md:tracking-wider  w-3/4 md:min-w-screens min-w-screen`}
+          className={`flex flex-col ${robotoSlab.className}  md:text-lg font-normal text-[12px] text-center  mt-4 my-auto  w-[70%] md:min-w-screens min-w-screen`}
         >
-          <h3 className="leading-6 md:leading-10">
-            Powerlines is an independent NGO advancing policy innovation and
-            <br className="hidden md:block" />
-            thought leadership within the energy community
-          </h3>
-          <h3 className="mt-3 md:whitespace-nowrap flex flex-row justify-center leading-6 sm:leading-10">
+          <p className="leading-6 md:leading-10 ">
+            PowerLines is an independent NGO advancing{" "}
+            <br className="md:hidden" />
+            policy innovation and <br className="hidden md:block" />
+            thought leadership{" "}
+            <br className="md:hidden" />
+            within the energy community.
+            <br className="mt-4" />
             Together, we can create a movement to{" "}
             <br className="block md:hidden" /> change the energy landscape.
-          </h3>
+          </p>
 
           {/* Scroll Button */}
           <section className="flex-1 justify-center mt-3 hidden md:flex md:flex-row ">
