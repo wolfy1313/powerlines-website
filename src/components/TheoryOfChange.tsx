e976035710a4b7fcdbee959f36bf0cbbb90acad0
Kevin Castro <kcapupp@gmail.com>
2024-03-27 23:41:41 -0400

    TOC card design match

--- src/components/TheoryOfChange.tsx
+++ src/components/TheoryOfChange.tsx
@@ -12,7 +12,7 @@ function TheoryOfChange() {
       <section className="flex flex-row justify-center">
         <section className="flex flex-col items-center justify-center">
           <h2
-            className={`font-bold tracking-wider text-2xl  text-center md:text-3xl mt-8 md:mt-28 ${cairo.className}`}
+            className={`font-bold tracking-wide text-2xl md:text-[40px] text-center  mt-8 md:mt-28 ${cairo.className}`}
           >
             THEORY OF CHANGE
           </h2>
@@ -20,15 +20,16 @@ function TheoryOfChange() {
       </section>
       <section className="flex flex-row flex-1 justify-center min-w-screen my-auto mb-10">
         <section
-          className={`flex flex-col ${robotoSlab.className} flex flex-col items-center  max-w-screen md:min-w-screen md:tracking-wider md:text-xl mt-2 lg:mt-10`}
+          className={`flex flex-col ${robotoSlab.className}  text-[15px] md:text-xl   font-light flex flex-col items-center  max-w-screen md:min-w-screen  mt-2 lg:mt-10`}
         >
           <h2 className=" max-w-screen text-center md:min-w-screen mb-10 mx-10 lg:leading-10">
             We aim to reshape the energy system by accelerating clean energy and
-            equity through utility regulation by targeting{' '}
-            <br className="block md:hidden" /> state{' '}
+            equity through utility regulation by targeting{" "}
+            <br className="block md:hidden" /> state{" "}
             <br className="hidden md:block" /> public utilities commissions. To
-            <br className="block md:hidden" /> tackle this challenge, PowerLines pursues{' '}
-            <br className="block md:hidden" /> a three-part theory of change.
+            <br className="block md:hidden" /> tackle this challenge, PowerLines
+            pursues <br className="block md:hidden" /> a three-part theory of
+            change.
           </h2>
 
           {/* Cards */}
@@ -39,14 +40,14 @@ function TheoryOfChange() {
               header="Political strategy"
               imageSrc="/TOCCardGroup1.png"
               href="/political-strategy"
-              checklist={['Appoints Champions', 'Elects Champions']}
+              checklist={["Appoints Champions", "Elects Champions"]}
             />
 
             <ToCCard
               header="Model Legislation"
               imageSrc="/TOCCardGroup2.png"
               href="/model-legislation"
-              checklist={['Passes Legislation', 'Lorem Ipsum']}
+              checklist={["Passes Legislation", "Lorem Ipsum"]}
             />
 
             <ToCCard
@@ -54,16 +55,16 @@ function TheoryOfChange() {
               imageSrc="/TOCCardGroup3.png"
               href="/community-engagement"
               checklist={[
-                'Youth Groups',
-                'The Public',
-                'Environmental Justice'
+                "Youth Groups",
+                "The Public",
+                "Environmental Justice",
               ]}
             />
           </section>
         </section>
       </section>
     </section>
-  )
+  );
 }
 
 export default TheoryOfChange
--- src/components/ToCCard.tsx
+++ src/components/ToCCard.tsx
@@ -8,54 +8,52 @@ const cairo = Cairo({ subsets: ['latin'] })
 const robotoSlab = Roboto_Slab({ subsets: ['latin'] })
 const ToCCard = ({ header, imageSrc, checklist, href }: ToCCardProps) => {
   return (
-    <div className='group'>
-      <div className="bg-white p-4 border-x-1 border-t-1 lg:border-x-2 lg:border-t-2 border-[#E8E7E7] rounded-t-lg lg:mt-5 flex lg:flex-col justify-center items-center lg:justify-between w-400 h-[117px] lg:h-[434px] text-center md:flex-grow md:flex-shrink md:basis-1/3">
-          {/* Card Image Section */}
-          <section className='flex justify-center items-center relative h-[49px] w-[56px] lg:h-[116px] lg:w-[120px] mx-auto'>
-            <div className="lg:mt-4 w-[50px] w-[40px]  lg:w-[110px] lg:h-[105px] rounded-[30px] bg-[#FFEEA975] group-hover:bg-[#D91D4C21] flex ">
-              <Image
-                src={imageSrc}
-                alt={`${header} icon`}
-                width={109}
-                height={109}
-                className='object-contain w-48 h-45 lg:w-[109px] lg:h-[109px]'
-              />
-            </div>
-          </section>
+    <div className="group">
+      <div className="bg-white p-4 border-x-1 border-t-1 lg:border-x-2 lg:border-t-2 border-[#E8E7E7] rounded-t-lg lg:mt-5 flex lg:flex-col justify-center items-center  w-400 h-[117px] lg:h-[434px] text-center  md:basis-1/3">
+        {/* Card Image Section */}
+        <section className="flex justify-center items-center relative h-[49px] w-[56px] lg:h-[116px] lg:w-[120px] md:mt-4 mx-auto">
+          <div className="lg:mt-4 w-[50px] w-[40px]  lg:w-[110px] lg:h-[105px] rounded-[30px] bg-[#FFEEA975] group-hover:bg-[#D91D4C21] flex ">
+            <Image
+              src={imageSrc}
+              alt={`${header} icon`}
+              width={109}
+              height={109}
+              className="object-contain w-48 h-45 lg:w-[109px] lg:h-[109px]"
+            />
+          </div>
+        </section>
 
         {/* Text Container Section*/}
-<section className="flex flex-col lg:mt-4 px-6 py-4 flex-grow">
-  <div className="flex lg:items-center">
-    {/* Invisible Aligner for Header Alignment */}
-    <div className="hidden lg:block lg:flex-grow-0 lg:flex-shrink-0 w-[21px]"></div> {/* Adjust width based on checkmark size + padding */}
+        <section className="flex flex-col lg:mt-6 px-6  flex-grow">
+          <div className="flex lg:items-center">
+            {/* Invisible Aligner for Header Alignment */}
+            <div className="hidden lg:block lg:flex-grow-0 lg:flex-shrink-0 w-[21px]"></div>{" "}
+            {/* Adjust width based on checkmark size + padding */}
+            {/* Header */}
+            <div
+              className={`font-bold text-[lg] lg:text-2xl lg:text-center ${cairo.className}`}
+            >
+              {header}
+            </div>
+          </div>
 
-    {/* Header */}
-    <div className={`font-bold text-lg lg:text-2xl lg:text-center ${cairo.className}`}>
-      {header}
-    </div>
-  </div>
-
-  {/* List Items Text */}
-  <ul className="list-inside text-xs lg:text-base lg:p-5 lg:space-y-4">
-    {checklist.map((listItem, index) => (
-      <li className="flex flex-row" key={index}>
-        <Image src="/checkmark.svg" height={20} width={20} alt="" />
-        <p className="pl-1 md:pl-3">{listItem}</p>
-      </li>
-    ))}
-  </ul>
-</section>
-
-
-       
+          {/* List Items Text */}
+          <ul className="list-inside text-xs lg:text-base lg:font-normal lg:px-9 lg:pt-6 lg:pb-0  lg:space-y-4">
+            {checklist.map((listItem, index) => (
+              <li className="flex flex-row" key={index}>
+                <Image src="/checkmark.svg" height={20} width={20} alt="" />
+                <p className="pl-1 md:pl-3">{listItem}</p>
+              </li>
+            ))}
+          </ul>
+        </section>
 
         {/* Call to action button Section */}
-        <section className="flex items-center justify-center lg:pb-3">
-          
+        <section className="flex items-center justify-center lg:mb-6">
           {/* Full CTA for lg screens and up */}
           <Link
             href={`${href}`}
-            className={`hidden lg:inline-flex items-center px-6 py-3 text-sm font-bold text-center text-[#D76C00] bg-main-white border-2 border-[#D76C00] rounded-md ${cairo.className}`}
+            className={`hidden lg:inline-flex items-center px-8 py-2 text-xl font-bold text-center text-[#D76C00] bg-main-white border border-[#D76C00] rounded-md ${cairo.className}`}
           >
             Learn More
             <Image
@@ -63,13 +61,16 @@ const ToCCard = ({ header, imageSrc, checklist, href }: ToCCardProps) => {
               alt=""
               height={5}
               width={28}
-              className="ml-2 fill-black"
+              className="ml-2 fill-black "
             />
           </Link>
 
           {/* Only arrow for smaller than lg screens */}
           <div className="lg:hidden">
-            <Link href={`${href}`} className={`flex items-center justify-center`}>
+            <Link
+              href={`${href}`}
+              className={`flex items-center justify-center`}
+            >
               <Image
                 src="/ToCCard-arrow.svg"
                 alt="Arrow"
@@ -80,12 +81,11 @@ const ToCCard = ({ header, imageSrc, checklist, href }: ToCCardProps) => {
             </Link>
           </div>
         </section>
-
       </div>
-      
+
       {/* bottom border interactive line */}
-      <div 
-        className='
+      <div
+        className="
         rounded-b-lg 
         h-[3px] 
         lg:h-[6px]  
@@ -97,10 +97,10 @@ const ToCCard = ({ header, imageSrc, checklist, href }: ToCCardProps) => {
         lg:to-[#FFD010] 
         group-hover:bg-gradient-to-r 
         group-hover:from-[#F5A020] 
-        group-hover:to-[#EF8529]'>
-      </div>
+        group-hover:to-[#EF8529]"
+      ></div>
     </div>
-  )
+  );
 }
 
 export default ToCCard
