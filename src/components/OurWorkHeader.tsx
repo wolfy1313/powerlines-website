72f542fa9a8150af1d7032dbd2463f34a933e3b9
jeremyHarrell <hillvulf@gmail.com>
2024-03-29 11:59:09 -0500

    adjust styling on OurWorkHeader.tsx

--- src/components/OurWorkHeader.tsx
+++ src/components/OurWorkHeader.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 function OurWorkHeader() {
   return (
     <section className=' min-w-full max-w-screen'>
-      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-64  bg-contain bg-right md:bg-inherit">
+      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-64 bg-contain bg-center xl:bg-left-bottom md:bg-center xl:bg-cover sm:bg-cover">
         <h1 className={`flex items-center font-bold align-middle p-16 text-2xl sm:text-4xl md:text-5xl font-main`}>OUR WORK</h1>
       </section>
     </section>
