e6da91e76256009b527281661e40a22659c90327
jeremyHarrell <hillvulf@gmail.com>
2024-04-01 14:42:14 -0500

    adjust header for non-mobile screens in OurWorkHeader.tsx

--- src/components/OurWorkHeader.tsx
+++ src/components/OurWorkHeader.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 function OurWorkHeader() {
   return (
-    <section className='min-w-full max-w-screen h-32'>
-      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-24 bg-contain bg-left-bottom xl:bg-left-bottom lg:bg-cover">
-        <h1 className={`items-center font-bold align-middle p-4 md:pt-32 pt-10 text-center text-2xl sm:text-4xl md:text-5xl font-main`}>OUR WORK</h1>
+    <section className='min-w-full max-w-screen h-32 sm:h-80'>
+      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-24 sm:h-64 bg-contain bg-left-bottom xl:bg-bottom xl:bg-cover ">
+        <h1 className={`items-center font-bold align-middle p-4 sm:p-6 pt-10 sm:pt-32 text-center text-2xl sm:text-5xl md:text-6xl xl:text-7xl font-main`}>OUR WORK</h1>
       </section>
     </section>
   )
