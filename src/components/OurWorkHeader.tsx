8ba3aeb0bcad397cfe1432fc9b7200f962d9b543
jeremyHarrell <hillvulf@gmail.com>
2024-04-01 14:28:22 -0500

    adjust header for mobile in OurWorkHeader.tsx

--- src/components/OurWorkHeader.tsx
+++ src/components/OurWorkHeader.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 function OurWorkHeader() {
   return (
-    <section className='min-w-full max-w-screen'>
-      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-64 bg-contain bg-left-bottom xl:bg-left-bottom lg:bg-cover">
-        <h1 className={`items-center font-bold align-middle p-20 md:pt-32 pt-48 text-center text-2xl sm:text-4xl md:text-5xl font-main`}>OUR WORK</h1>
+    <section className='min-w-full max-w-screen h-32'>
+      <section className=" flex bg-no-repeat bg-[url('/OurWorkHeaderBackground.png')] h-24 bg-contain bg-left-bottom xl:bg-left-bottom lg:bg-cover">
+        <h1 className={`items-center font-bold align-middle p-4 md:pt-32 pt-10 text-center text-2xl sm:text-4xl md:text-5xl font-main`}>OUR WORK</h1>
       </section>
     </section>
   )
